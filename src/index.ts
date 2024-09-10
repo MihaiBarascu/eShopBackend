c6b86becc4f9c655323e65255d4e9b2afdc2a0fe
MihaiBarascu <mihaialex_barascu@yahoo.com>
2024-09-10 22:48:35 +0300

    configure nodemon with docker

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("ce faci?");
+});
+console.log("ceau");
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
