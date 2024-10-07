876d62be1f2da5d1a0cd14312f56dc901b7e12c5
MihaiBarascu <mihaialex_barascu@yahoo.com>
2024-10-07 21:03:58 +0300

    switch to prod

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,16 +4,22 @@ import { info, error as logError } from "../utils/logger";
 
 dotenv.config();
 
+console.log(process.env.DB_PORT, process.env.DB_HOST);
+
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT) || 3306,
+  // host: process.env.DB_HOST || "127.0.0.1",
+  host: "127.0.0.1",
+
+  port: Number(process.env.DB_PORT) || 3308,
+  // port: Number(process.env.DB_PORT) || 3306,
   username: "root",
   password: "root",
   database: "eShop",
   migrations: ["src/database/migrations/*.{js,ts}"],
   logging: process.env.ORM_LOGGING === "true",
-  entities: ["src/database/entity/**/*.{js,ts}"],
+  // entities: ["src/database/entity/**/*.{js,ts}"],
+  entities: ["dist/database/entity/**/*.js"],
   synchronize: true,
   subscribers: [],
 });
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export const PORT = process.env.APP_PORT || 3000;
 export const DB_CONNECTION = process.env.DB_CONNECTION || "mysql";
 export const DB_HOST = process.env.DB_HOST || "mysql";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_PORT = process.env.DB_PORT || 3308;
 export const DB_USERNAME = process.env.DB_USERNAME || "eShop";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "eShop";
 export const DB_DATABASE = process.env.DB_DATABASE || "eShop";
